57c13f240cfd14f4d8d574db3e133b978295c12b
vivekdutta123 <vivekdutta215@gmail.com>
2024-06-29 18:25:08 +0000

    Sat, Jun 29, 2024, 11:55 PM +05:30

--- vivek.py
+++ vivek.py
@@ -1,14 +1,17 @@
+#max and min of three numbers using function
 def find_maximum(a, b, c):
-    a=int(input("enter a number:-"))
-    b=int(input("enter a number:-"))
-    c=int(input("enter a number:-"))
- if a>=b and a>=c:
-    print(" a is maximum")
- elif b>=a and b>=c:
-    print(" b is maximum")
- elif c>=a and c>=b:
-    print("c is maximum ")
- else:
-    ("minimum")
+    if a >= b and a >= c:
+        print("a is maximum")
+    elif b >= a and b >= c:
+        print("b is maximum")
+    elif c >= a and c >= b:
+        print("c is maximum")
+    else:
+        print("All numbers are equal")
 
+# Example usage
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+c = int(input("Enter the third number: "))
 
+find_maximum(a, b, c)
