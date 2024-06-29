b50c41df0a2299ca28cd78b9bcf5ee173a0c49a4
vivekdutta123 <vivekdutta215@gmail.com>
2024-06-29 18:20:31 +0000

    Sat, Jun 29, 2024, 11:50 PM +05:30

--- vivek.py
+++ vivek.py
@@ -1,13 +1,14 @@
 def find_maximum(a, b, c):
-a=int(input("enter a number:-"))
-b=int(input("enter a number:-"))
-c=int(input("enter a number:-"))
-if a>=b and a>=c:
+    a=int(input("enter a number:-"))
+    b=int(input("enter a number:-"))
+    c=int(input("enter a number:-"))
+ if a>=b and a>=c:
     print(" a is maximum")
-elif b>=a and b>c:
+ elif b>=a and b>=c:
     print(" b is maximum")
-elif c>=a and c>=b:
+ elif c>=a and c>=b:
     print("c is maximum ")
-else:
+ else:
     ("minimum")
+
 
