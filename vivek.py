eb41b6757d52a4c9cc681c4db2a5ea11e497ba8a
vivekdutta123 <vivekdutta215@gmail.com>
2024-06-29 17:54:19 +0000

    Sat, Jun 29, 2024, 11:24 PM +05:30

--- vivek.py
+++ vivek.py
@@ -2,7 +2,10 @@
 a=int("enter a number:-")
 b=int("enter a number:-")
 c=int("enter a number:-")
-if a>b:
-    print("maximum")
-elif:
+if a>b or a>c:
+    print(" a is maximum")
+elif b>a or b>c:
+    print(" b is maximum")
+elif c>a or c>b:
+    print(")
 
