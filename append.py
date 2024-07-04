e4a226857c996b1b05091102bb782b60e453f407
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-04 06:49:49 +0000

    Thu, Jul 4, 2024, 12:19 PM +05:30

--- append.py
+++ append.py
@@ -1,9 +1,9 @@
 #file=open("myfile.txt","w")
 #file.write("more information")
 #file.close()
-file=open("distinationfile.txt","w")
-file.write("all copid information")
-file.close()
+#file=open("distinationfile.txt","w")
+#file.write("all copid information")
+#file.close()
 file_to_read="myfile.txt"
 write_to_file="destination.txt"
 file=open(file_to_read,"r")
