f7a5d26ac97034cb17e3c1d75d4eb165dceaff5c
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-04 03:45:37 +0000

    Thu, Jul 4, 2024, 9:15 AM +05:30

--- assignment.py
+++ assignment.py
@@ -1,9 +1,5 @@
 string_list = ["BANANA ", "APPLE", "CHERRY"]
-
-# Convert all strings to lowercase
- for s in string_list:
-   lower_string=s.lower()
-
-sorted_strings = sorted(lowercase_strings)
-
-print(sorted_strings)
+for s in string list():
+ lower_string=s.lower()
+ sorted_strings = sorted(lowercase_strings)
+ print(sorted_strings)
