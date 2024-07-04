cc0da64fb6643b99d59652fae4c5cf03ef326acb
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-04 05:53:24 +0000

    Thu, Jul 4, 2024, 11:23 AM +05:30

--- append.py
+++ append.py
@@ -1,8 +1,3 @@
 file_to_read="Readdata.txt"
 write_to_file="writedata.txt"
-file=open(file_to_read,"r")
-data=file.read()
-file.close
-with open(write_to_file,"a")as file:
-    file.write(data)
-print("completed")
+file
