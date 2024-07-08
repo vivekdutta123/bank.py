ed1b5cd26e610047f933dcfde4008cbc38a56871
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-08 03:50:13 +0000

    Mon, Jul 8, 2024, 9:20 AM +05:30

--- faces/faces.py
+++ faces/faces.py
@@ -2,6 +2,7 @@ def main():
     message=input("enter a message -")
     result=convert(message)
 print(result)
+
 def convert(message):
     msg1=message.replace(":)","🙂")
     msg2=msg1.replace(":(","🙁")
