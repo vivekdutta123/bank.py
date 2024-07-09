e1a773036ac7d1282953f9fa6dd82f7d13feeb14
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-09 04:53:35 +0000

    Tue, Jul 9, 2024, 10:23 AM +05:30

--- sum.py
+++ sum.py
@@ -1,8 +1,8 @@
-#def sum():
-   # a = int(input("Enter the number of values to sum: "))
-    #total_sum = 0
-    #for _ in range(a):
-       # b = float(input("Enter a number: "))
-       # total_sum += b
-    #print("Sum of entered numbers:", total_sum)
-#sum()
+def sum():
+    a = int(input("Enter the number of values to sum: "))
+    total_sum = 0
+    for _ in range(a):
+        b = float(input("Enter a number: "))
+        total_sum += b
+    print("Sum of entered numbers:", total_sum)
+sum()
