3774c937100ab178cc4c63e90560dd0dc5fed7ee
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-09 14:46:02 +0000

    Tue, Jul 9, 2024, 8:16 PM +05:30

--- even.py
+++ even.py
@@ -6,6 +6,7 @@ even_numbers(1, 10)
 
 #sorted string
 def sort_hypen(string_list):
+ words=string_list.split("-")
  string_list=green-red-yellow-black-white
  sorted_string=string_list(sorted)
  print(sorted_string)
