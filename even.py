73a1c942cc214ed391d6737abd8af5e2a7dca5e0
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-11 16:12:34 +0000

    Thu, Jul 11, 2024, 9:42 PM +05:30

--- even.py
+++ even.py
@@ -4,18 +4,3 @@ def even_numbers(a,b):
             print(number)
 even_numbers(1, 10)
 
-# Get user input
-sample_items = input("Enter a hyphen-separated sequence of words: ")
-
-# Split the input sequence into words
-words = sample_items.split('-')
-
-# Sort the list of words
-sorted_words = sorted(words)
-
-# Join the sorted list of words into a single string with hyphens
-sorted_sequence = '-'.join(sorted_words)
-
-# Print the sorted sequence
-print(sorted_sequence)
-
--- hypen.py
+++ hypen.py
@@ -1,7 +1,15 @@
-def sort_hyphen(string_list):
-    words = string_list.split("-")
-    sorted_words = sorted(words)
-    sorted_string = "-".join(sorted_words)
-    print(sorted_string)
+# Get user input
+sample_items = input("Enter a hyphen-separated sequence of words: ")
 
+# Split the input sequence into words
+words = sample_items.split('-')
+
+# Sort the list of words
+sorted_words = sorted(words)
+
+# Join the sorted list of words into a single string with hyphens
+sorted_sequence = '-'.join(sorted_words)
+
+# Print the sorted sequence
+print(sorted_sequence)
 
