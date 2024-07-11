8363b9f88ba2bf3576c78318df8d36591f440623
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-11 16:10:12 +0000

    Thu, Jul 11, 2024, 9:40 PM +05:30

--- even.py
+++ even.py
@@ -4,17 +4,18 @@ def even_numbers(a,b):
             print(number)
 even_numbers(1, 10)
 
-#sorted string
-sample_string = input("Enter a space-separated sequence of words: ")
+# Get user input
+sample_items = input("Enter a hyphen-separated sequence of words: ")
 
-# Split the input string into words
-words = sample_string.split()
+# Split the input sequence into words
+words = sample_items.split('-')
 
 # Sort the list of words
 sorted_words = sorted(words)
 
-# Join the sorted list of words into a single string with spaces
-sorted_string = " ".join(sorted_words)
+# Join the sorted list of words into a single string with hyphens
+sorted_sequence = '-'.join(sorted_words)
 
-# Print the sorted string
-print(sorted_string)
+# Print the sorted sequence
+print(sorted_sequence)
+
