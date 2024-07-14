493b3f3af873b9049d04c507c68e12469117332d
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-14 16:17:10 +0000

    Sun, Jul 14, 2024, 9:47 PM +05:30

---
+++ evenodd.py
@@ -0,0 +1,10 @@
+def check_odd_or_even(number):
+    if number % 2 == 0:
+        return "Even"
+    else:
+        return "Odd"
+
+# Test the function
+num = int(input("Enter a number: "))
+result = check_odd_or_even(num)
+print(f"The number {num} is {result}.")
