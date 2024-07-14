38f5aa20c63e63428095e4d5057496e89ccaa0d4
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-14 16:38:21 +0000

    Sun, Jul 14, 2024, 10:08 PM +05:30

--- hours.py
+++ hours.py
@@ -1,14 +1,17 @@
-# Input for hours worked and hourly rate
+
+def compute_pay(hours, rate):
+    if hours > 40:
+        regular_pay = 40 * rate
+        overtime_pay = (hours - 40) * rate * 1.5
+        total_pay = regular_pay + overtime_pay
+    else:
+        total_pay = hours * rat
+
+
 hours = float(input("Enter hours worked: "))
 rate = float(input("Enter hourly rate: "))
 
-# Compute regular and overtime pay
-if hours > 40:
-    regular_pay = 40 * rate
-    overtime_pay = (hours - 40) * rate * 1.5
-    total_pay = regular_pay + overtime_pay
-else:
-    total_pay = hours * rate
+total_pay = compute_pay(hours, rate)
 
 # Output the total pay
 print("Total pay: $", total_pay)
