f4a7646dfe7665a9b20e1487282de87c2a199ee5
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-27 07:11:50 +0000

    Sat, Jul 27, 2024, 12:41 PM +05:30

---
+++ form.py
@@ -0,0 +1,15 @@
+filename = input("Enter the filename: ")
+
+fh = open(filename,'r')
+for line in fh:
+    fx = line.rstrip()
+    words = fx.split()
+    for word in words:
+        if word not in unique_words:
+            unique_words.append(word)
+fh.close()
+
+unique_words.sort()
+print("Unique words in alphabetical order:")
+for word in unique_words:
+    print(word)
