4ee6b30de19fb0479505e87b05efcf3bcbc8c11e
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-27 07:26:21 +0000

    Sat, Jul 27, 2024, 12:56 PM +05:30

--- form.py
+++ form.py
@@ -1,10 +1,8 @@
 filename = input("Enter the filename: ")
-
 fh = open(filename,'r')
 for line in fh:
     fx = line.rstrip()
-    if line startwith("from")
-    words=line.split()
-    for word in words:
-       for word in words[1:]:
-                print(word)
+    if fx startwith("from"):
+     words=fx.split()
+     for word in words[1:]:
+      print(word)
