b40cb2d889a7e5c0623dd68d41dc761bc2ac241b
vivekdutta123 <vivekdutta215@gmail.com>
2024-07-27 07:38:30 +0000

    Sat, Jul 27, 2024, 1:08 PM +05:30

--- form.py
+++ form.py
@@ -1,8 +1,16 @@
-filename = input("Enter the filename: ")
-fh=open(filename)
-for line in fh:
-    fx = line.rstrip()
-    if fx.startswith("from"):
-        words = fx.split()
-        for word in words[1:]:
-                print(word)
+# Open the file
+filename = 'yourfile.txt'  # Replace with your file name
+
+try:
+    with open(filename, 'r') as file:
+        for line in file:
+            # Check if the line starts with "From"
+            if line.startswith('From'):
+                # Split the line into words
+                words = line.split()
+                # Print each line from the second word onward
+                if len(words) > 1:
+                    print(' '.join(words[1:]))
+except FileNotFoundError:
+    print(f"File {filename} not found.")
+
