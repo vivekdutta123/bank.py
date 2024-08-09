08bb0e9dcf150d2bb7f6c3226e8bed0ef910053a
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-09 07:08:53 +0000

    Fri, Aug 9, 2024, 12:38 PM +05:30

--- deep/deep.py
+++ deep/deep.py
@@ -1,1 +1,5 @@
-answer=int(" what is the answer to the Great Question of Life, the Universe and Everything")
+answer=int(" what is the answer to the Great Question of Life, the Universe and Everything?")
+if answer=="42":
+    print("yes")
+if answer=="":
+    print("yes")
