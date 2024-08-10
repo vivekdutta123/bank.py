677e3457e666aa42195bcb48a462dc074139c41c
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-10 03:53:27 +0000

    Sat, Aug 10, 2024, 9:23 AM +05:30

--- bank/bank.py
+++ bank/bank.py
@@ -2,6 +2,7 @@ answer=input("greeting:")
 new_answer=answer.lower().strip()
 if "hello" new_answer:
     print("$0")
-elif "h"=new_number[0]:
-    print("$20")
+elif "h"=new_answer[0]:
+     print("$20")
+
 
