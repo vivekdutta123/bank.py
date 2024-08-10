33bd64a57e8c8061dfc7ea2e3410c3880c739201
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-10 06:19:31 +0000

    Sat, Aug 10, 2024, 11:49 AM +05:30

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -3,13 +3,13 @@ new_filename=filename.lower()
 if ".gif" in new_filename:
     print("image/.gif")
 elif ".png" in new_filename:
-    print("image/.png")
+    print("image/png")
 elif ".jpg" in new_filename:
-    print("image/.jpeg")
+    print("image/jpeg")
 elif ".jpeg" in new_filename:
     print("image/.jpeg")
 elif ".pdf" in new_filename:
-    print("application/.pdf")
+    print("application/pdf")
 elif ".zip" in new_filename:
     print("application/.zip")
 elif ".txt" in new_filename:
