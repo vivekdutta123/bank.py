439cb5b2f002142898ec628fb6ee2f04cb1fd412
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-10 06:00:04 +0000

    Sat, Aug 10, 2024, 11:30 AM +05:30

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,2 +1,16 @@
 filename=input("file extention:")
 new_filename=filename.lower()
+if .gif in new_filename:
+    print("image/.gif")
+if .png in new_filename:
+    print("image/.png")
+if .jpg in new_filename:
+    print("image/.jpg")
+if .jpeg in new_filename:
+    print("image/.jpeg")
+if .pdf in new_filename:
+    print("application/.pdf")
+if .zip in new_filename:
+    print("application/.zip")
+if .txt in new_filename:
+    print("txt/.plain")
