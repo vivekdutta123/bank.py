4d518b086b41b082eed02470e8f1f5be64515e51
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-10 04:01:59 +0000

    Sat, Aug 10, 2024, 9:31 AM +05:30

--- bank/bank.py
+++ bank/bank.py
@@ -2,7 +2,7 @@ answer=input("greeting:")
 new_answer=answer.lower().strip()
 if 'hello' in  new_answer:
     print("$0")
-elif 'h'== new_answer[0]:
+elif new_answer[0]:
     print("$20")
 else:
     print("$100")
