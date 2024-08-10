d050c915b7f109402e1c6fcb77b6fdaa3e0b5600
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-10 06:07:14 +0000

    Sat, Aug 10, 2024, 11:37 AM +05:30

--- extensions/extensions.py
+++ extensions/extensions.py
@@ -1,18 +1,18 @@
 filename=input("file extention:")
 new_filename=filename.lower()
-if .gif in new_filename:
+if ".gif" in new_filename:
     print("image/.gif")
-elif .png in new_filename:
+elif ".png" in new_filename:
     print("image/.png")
-elif .jpg in new_filename:
+elif ".jpg" in new_filename:
     print("image/.jpeg")
-elif .jpeg in new_filename:
+elif ".jpeg" in new_filename:
     print("image/.jpeg")
-elif .pdf in new_filename:
+elif ".pdf" in new_filename:
     print("application/.pdf")
-if .zip in new_filename:
+if ".zip" in new_filename:
     print("application/.zip")
-elif .txt in new_filename:
+elif ".txt" in new_filename:
     print("txt/.plain")
 else:
-    print(")
+    print("application/octet-stream")
