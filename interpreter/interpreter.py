5b5b7df39dc68b4c73acb9c6b6334ce43b4dcdd7
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-11 15:15:50 +0000

    Sun, Aug 11, 2024, 8:45 PM +05:30

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -4,10 +4,10 @@ new_x = float(x)
 new_z = float(z)
 if y == "+":
     result=new_x+new_z
-if y == "-":
+elif y == "-":
     result=new_x-new_z
-if y == "*":
+elif y == "*":
     result=new_x*new_z
-if y == "/":
+elif y == "/":
     result=new_x/new_z
 print(round(result,1))
