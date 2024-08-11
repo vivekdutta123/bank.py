de78384ff2ed50d3ea85dbd4f3c48ba1da6ad63d
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-11 15:09:59 +0000

    Sun, Aug 11, 2024, 8:39 PM +05:30

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,7 +1,7 @@
 expression=input("expression:")
-x, y, z=expression.split(" ")
-new_x=float(x)
-new_z=float(z)
+x, y, z = expression.split(" ")
+new_x = float(x)
+new_z = float(z)
 if y == "+":
     result=new_x+new_z
 if y == "-":
