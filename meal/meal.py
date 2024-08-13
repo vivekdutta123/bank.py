5fb668f7b7d991dfb16b7fe5e5efe49c7df34758
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-13 03:53:41 +0000

    Tue, Aug 13, 2024, 9:23 AM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,17 +1,18 @@
 def main():
-    answer=input("what time is it")
-    time=convert(answer)
-if time>=7 and time<=8:
-    print("breakfast time")
-elif time>=12 and time<=13:
-    print("lunch time")
-elif time>=18 and time<=19:
-    print("dinner time")
-else:
-    print("you are out of time")
+    answer = input("What time is it? (HH:MM) ")
+    time = convert(answer)
+    if 7 <= time <= 8:
+        print("Breakfast time")
+    elif 12 <= time <= 13:
+        print("Lunch time")
+    elif 18 <= time <= 19:
+        print("Dinner time")
+    else:
+        print("You are out of time")
+
 def convert(time):
     hours, minutes = time.split(":")
-    new_minute=float(minutes)/60
+    new_minute = float(minutes) / 60  
     return float(hours) + new_minute
 
 if __name__ == "__main__":
