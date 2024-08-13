4d106d0684a9cb41b31aa68598ebc1aab3b3f3cd
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-13 03:32:51 +0000

    Tue, Aug 13, 2024, 9:02 AM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -1,8 +1,16 @@
 def main():
     answer=input("what time is it")
     time=convert(answer)
-
-
+if time>=7 and time<=8:
+    print("breakfast time")
+elif time>=12 and time<=13:
+    print("lunch time")
+elif time>=12 and time<=13:
+    print("lunch time")
+elif time>=18 and time<=19:
+    print("dinner time")
+else:
+    print("you are out of time")
 def convert(time):
     hours, minutes = time.split(":")
     new_minute=float(minute)/60
