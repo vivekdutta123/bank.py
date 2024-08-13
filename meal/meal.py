dcf2d1f65237f18c34d4299e3cf70db475c611b4
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-13 03:34:18 +0000

    Tue, Aug 13, 2024, 9:04 AM +05:30

--- meal/meal.py
+++ meal/meal.py
@@ -5,8 +5,6 @@ if time>=7 and time<=8:
     print("breakfast time")
 elif time>=12 and time<=13:
     print("lunch time")
-elif time>=12 and time<=13:
-    print("lunch time")
 elif time>=18 and time<=19:
     print("dinner time")
 else:
