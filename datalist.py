64dfb566756d3e17952c1fa6863d2147e3268dcf
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-17 08:29:44 +0000

    Sat, Aug 17, 2024, 1:59 PM +05:30

--- datalist.py
+++ datalist.py
@@ -1,3 +1,8 @@
-datalist=[1234,11.23,1+2j]
-for item in datalist:
-    print("type of item is",item,type(item))
+#datalist=[1234,11.23,1+2j]
+#for item in datalist:
+    #print("type of item is",item,type(item))
+f=[]
+a=[]
+while ch ==y:
+    print ch.lower()
+    item=int(input("enter a item of a list"))
