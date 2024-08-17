b5ba3a5cf2c82fa42189ed2fa8e66b73333512a2
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-17 07:12:55 +0000

    Sat, Aug 17, 2024, 12:42 PM +05:30

--- slice.py
+++ slice.py
@@ -14,6 +14,6 @@ print("List of elements:", n)
 a=len(n)
 for i in range(n):
     for j in range(0,n-i-1):
-    if nst[j]> nst[j+1]:
-    nst[j]+nst[j+1]=nst[j+1]+nst[j]
+      if nst[j]> nst[j+1]:
+         nst[j]+nst[j+1]=nst[j+1]+nst[j]
 print("after sorting of list".nst)
