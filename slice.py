3211cc54acc993840904ceaa32ca054d25cbde49
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-17 03:53:49 +0000

    Sat, Aug 17, 2024, 9:23 AM +05:30

--- slice.py
+++ slice.py
@@ -1,3 +1,3 @@
 my_list = ["vivek", "tarun", "bhawesh", "gyanti"]
-number = my_list[2:4]  # The index 4 is exclusive, so this will include elements at indices 2 and 3.
+number = my_list[0]  # The index 4 is exclusive, so this will include elements at indices 2 and 3.
 print(number)  # Output: ['bhawesh', 'gyanti']
