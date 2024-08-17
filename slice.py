02755031942af6d7f235c3ed5d681fe200aff1a2
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-17 06:53:09 +0000

    Sat, Aug 17, 2024, 12:23 PM +05:30

--- slice.py
+++ slice.py
@@ -1,7 +1,7 @@
 limit=int(input("enter a number:"))
 n=[]
 #list of element inputed by user
-for_in range(1,a+1):
+for_in range(1,element+1):
   element=int(input("enter a element"))
   n.append("element")
 print("list of element",n)
