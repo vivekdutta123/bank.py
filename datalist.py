e215b2415feea9ae1a67f355388902465eb4ddd7
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-17 13:41:04 +0000

    Sat, Aug 17, 2024, 7:11 PM +05:30

--- datalist.py
+++ datalist.py
@@ -1,23 +1,23 @@
 #datalist=[1234,11.23,1+2j]
 #for item in datalist:
     #print("type of item is",item,type(item))
-f = []
-a = []
-ch = "y"
+#f = []
+#a = []
+#ch = "y"
 
-while ch.lower() == "y":
-    item = int(input("Enter an item for the list: "))
-    a.append(item)
-    ch = input("Do you want to enter another element (yes/no)? ").lower()
+#while ch.lower() == "y":
+   # item = int(input("Enter an item for the list: "))
+    #a.append(item)
+    #ch = input("Do you want to enter another element (yes/no)? ").lower()
 
-print("List is", a)
+#print("List is", a)
 
 # To print factorial
-fact = []
-for i in a:
-    f = 1
-    for j in range(1, i + 1):
-        f *= j
-    fact.append(f)
+#fact = []
+#for i in a:
+    #f = 1
+    #for j in range(1, i + 1):
+    # f *= j
+    #fact.append(f)
 
-print("Factorial of the numbers is", fact)
+#print("Factorial of the numbers is", fact)
