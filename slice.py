cde8fcc4fa14ab23c165231895942b067db9e642
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-17 05:23:53 +0000

    Sat, Aug 17, 2024, 10:53 AM +05:30

--- slice.py
+++ slice.py
@@ -1,4 +1,1 @@
-my_list = ["vivek", "tarun", "bhawesh", "gyanti"]
-for i in range(len(my_list)):
-    my_list[i] = my_list[i] * 2  # This will repeat each string
-print(my_list)  # Output: ['vivekvivek', 'taruntarun', 'bhaweshbhawesh', 'gyantigyanti']
+a=eval(input("enter a limit"))
