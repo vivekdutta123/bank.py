f8004d6ad8d7935ea61f27886800308814ca0f4f
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-18 06:39:18 +0000

    Sun, Aug 18, 2024, 12:09 PM +05:30

--- file.py
+++ file.py
@@ -1,6 +1,6 @@
-fhand = open("Rj.txt")
+fhand = open("destination.txt")
 count = 0
 for line in fhand:
-    if line.startswith("the:"):
+    if line.startswith("Hellow:"):
         print(line)
 fhand.close()  # Remember to close the file when done
