c996b0efc4c32cf7a53b999321511b8fe06ec016
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-18 07:19:57 +0000

    Sun, Aug 18, 2024, 12:49 PM +05:30

--- file.py
+++ file.py
@@ -12,12 +12,6 @@
 filename = input("Enter the filename: ")
 fh = open(filename)
 count=0
-# Loop through each line in the file
 for line in fh:
     count=count+1
-    # Check if the line starts with "Hellow"
-    if line.startswith("Hellow"):
-        print(line.strip())  # .strip() removes leading/trailing whitespace
-
-# Close the file after processing
-fh.close()
+print("line count:",count )
