5501b5812dfaddeebf2d5ab02e678b4715c032ed
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-18 08:17:06 +0000

    Sun, Aug 18, 2024, 1:47 PM +05:30

--- file.py
+++ file.py
@@ -9,9 +9,20 @@
 
 # Close the file after processing
 #fh.close()
+#filename = input("Enter the filename: ")
+#fh = open(filename)
+#count=0
+#for line in fh:
+    #count=count+1
+#print("line count:",count )
 filename = input("Enter the filename: ")
 fh = open(filename)
-count=0
+
+# Loop through each line in the file
 for line in fh:
-    count=count+1
-print("line count:",count )
+    # Check if the line starts with "Hellow"
+    if line.startswith("Hellow"):
+        print(line.strip())  # .strip() removes leading/trailing whitespace
+
+# Close the file after processing
+fh.close()
