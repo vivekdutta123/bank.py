95c6bec74a457c05141b0c3caf75753d1eb9df54
vivekdutta123 <vivekdutta215@gmail.com>
2024-08-18 06:02:37 +0000

    Sun, Aug 18, 2024, 11:32 AM +05:30

--- file.py
+++ file.py
@@ -1,2 +1,4 @@
-fhand=open("Rj.txt")
-print(fhand)
+fhand = open("Rj.txt")
+content = fhand.read()  # Read the content of the file
+print(content)  # Print the content
+fhand.close()  # Close the file
